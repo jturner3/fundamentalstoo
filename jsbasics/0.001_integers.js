c21a1db21278eec96aeee78fff8540e6deb846ff
John <317horus@gmail.com>
2016-09-21 10:31:58 -0400

    the fundamentals

---
+++ jsbasics/0.001_integers.js
@@ -0,0 +1,102 @@
+// this is camel casing when naming variables//
+
+var num = 7;
+var newNum = 21; //this is camel casing
+var newernum = 32;//this is snake casing
+
+console.log(num&&newNum*newernum)
+
+// You can cosole.log wherever you want after putting in variables//
+
+var num = 8;
+var dumbNUm = 21;
+
+console.log(num*dumbNUm)
+
+//console.log() prints to the console
+
+console.log(5 + 6);
+
+//ToDo - make a subtraction, multiplication and division console.log()
+
+// console.log(76 - 54);
+// console.log(21 * 4);
+// console.log(45/9);
+
+//Modulus Operator 
+
+console.log(10 % 5); //returns zero
+
+console.log(12 % 5); //returns two
+
+//Todo - make a modulus that returns zero and another that returns a remainder
+
+console.log(25 % 5);
+console.log(29 % 5);
+
+//ToDo - try to make a variable that is assigned an operation and console.log
+//example var num = 1 + 1
+
+//To Do - try to assign numbers to different variables and perform an opertion to them
+//example - var a=5; var b=10; console.log(a*b)
+
+var num = 4 * 3;
+console.log(num)
+
+var cray = 6;
+var numNum = 3;
+console.log(cray * numNum);
+
+
+//ToDo - try to assign numbers to two different variables then assign 
+//any operation on these two variables to a new variable
+
+var a = 4
+var b = 2
+var c = a + b
+console.log(c) 
+
+//reassigning values
+
+var newNum = 10;
+newNum = newNum + 1;
+console.log(newNum)
+
+newNum++; //shortcut for newNUm = newNum +1
+console.log(newNum)
+
+newNum = newNum - 1; //shortcut for newNum = newNum - 1
+console.log(newNum)
+
+newNum--
+console.log(newNum)
+
+newNum = newNum + 10;
+console.log(newNum)
+
+newNum += 10; //shortcut for writing newNum = newNum + 10
+console.log(newNum);
+
+//TODO - take what you know from addition and apply it to subtraction back to 10
+
+newNum -= 10;
+console.log(newNum);
+
+newNum -= 10;
+console.log(newNum);
+
+newNum *= 10;
+console.log(newNum)
+
+newNum /= 10;
+console.log(newNum)
+
+
+
+
+
+
+
+
+
+
---
+++ jsbasics/0.00_setup.js
@@ -0,0 +1,9 @@
+var x = 21;
+console.log(x);
+
+var x = 20;
+var y = 4;
+
+console.log(x&&y);
+
+
